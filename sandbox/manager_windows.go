609d7349590fe6b21211654b52198de9899fe4f9
xSaVageAU <jessejd1997@gmail.com>
2026-07-02 02:35:27 +0930

    Tidy up sandbox package: remove dead session code/SQLite dependency and unused helpers

--- sandbox/manager_windows.go
+++ sandbox/manager_windows.go
@@ -2,12 +2,3 @@
 
 package sandbox
 
-import "os/exec"
-
-func isUserInGroup(groupName string) (bool, int) {
-	return false, 0
-}
-
-func configureCmdGid(cmd *exec.Cmd, gid int) {
-	// No-op on Windows
-}
